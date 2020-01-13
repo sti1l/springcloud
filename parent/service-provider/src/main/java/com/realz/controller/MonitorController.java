3f9658983bbf2d3ed4cd9a037a10c2e0199244d5
still <386654748@qq.com>
2020-01-13 09:14:44 +0800

    删除example包下文件.

--- parent/service-provider/src/main/java/com/realz/controller/MonitorController.java
+++ parent/service-provider/src/main/java/com/realz/controller/MonitorController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 访问监测测试Controller类
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MonitorController {
 	@RequestMapping("/monitorMain")
-	public void MonitorMain(HttpServletRequest req,
+	public @ResponseBody String MonitorMain(HttpServletRequest req,
 			HttpServletResponse rep) throws IOException {
 		// 输出到页面
 		//rep.getWriter().write("1111");
@@ -30,5 +31,7 @@ public class MonitorController {
 		rep.setContentType("text/html;charset=utf-8");
 		PrintWriter writer = rep.getWriter();
 		writer.print("<div style='color:red'>难道说</div>");
+		
+		return "1111";
 	}
 }
